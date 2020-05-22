2866c1ea240b02eecf5c57874e3691f7dc36426d
fdznoriega <andresfdz7@gmail.com>
2020-05-22 12:34:08 -0400

    uses jikan api to search for show and take top result

--- commands/rec.js
+++ commands/rec.js
@@ -1,15 +1,31 @@
+const jikanjs = require('jikanjs');
 
 module.exports = {
   name: 'rec',
   description: 'Adds show to recommendations',
   args: 'true',
   execute(message, args) {
+    // assume argument is ONE show
+    const name = args.toString().replace(',', ' ');
+    // try search for the show on MAL
+    jikanjs.search('anime', name)
+    .then( (response) => {
+      // fetch top result
+      topResult = response.results[0];
+      message.reply(`is this it?\n${topResult.title}\n${topResult.url}`);
+      // get confirmation from user
+    })
+    .catch( (err) => {
+      console.error(err);
+    });
+    //
 
 
 
 
-
-    message.reply('your recommendation has been added.');
+    // add item to message
+    // notify
+    // message.reply('your recommendation has been added.');
 
   }
 }
